7343ec96b9bc7bf0a42f05bc84d0e40a765fc987
krehwell <fahrimuham001@gmail.com>
2021-06-09 08:02:12 +0800

    added basic providers, controllers, and services

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.model';
+
+@Controller('tasks')
+export class TasksController {
+    constructor(private taskService: TasksService) {}
+
+    @Get()
+    getAllTasks(): Task[] {
+        return this.taskService.getAllTasks();
+    }
+
+    @Post()
+    createTask(@Body() { title, description }): Task {
+        return this.taskService.createTask(title, description);
+    }
+}
