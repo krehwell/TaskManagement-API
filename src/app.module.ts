b2e6fd8967600b5c75cd5909fd5ed64d1c5f1a5a
krehwell <fahrimuham001@gmail.com>
2021-08-15 11:04:32 +0800

    wrap config to .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,28 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            envFilePath: `.env.stage.${process.env.STAGE}`,
+        }),
         TasksModule,
-        TypeOrmModule.forRoot({
-            type: 'postgres',
-            port: 5432,
-            username: 'postgres',
-            password: 'postgres',
-            database: 'task-management',
-            autoLoadEntities: true,
-            synchronize: true,
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => {
+                return {
+                    type: 'postgres',
+                    port: configService.get('DB_PORT'),
+                    username: configService.get('DB_USERNAME'),
+                    password: configService.get('DB_PASSWORD'),
+                    database: configService.get('DB_DATABASE'),
+                    autoLoadEntities: true,
+                    synchronize: true,
+                };
+            },
         }),
         AuthModule,
     ],
